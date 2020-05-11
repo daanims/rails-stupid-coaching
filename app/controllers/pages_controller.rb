eb5b5f2c79625afd0b81db2150d8e36dce92f0c1
Daniela Santana <daanims@gmail.com>
2020-05-11 14:46:50 -0400

    looks ugly, but it works

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,16 @@
 class PagesController < ApplicationController
   def ask
-    # code
+  end
+
+  def answer
+    # puts params["question"]
+    @question = params['question']
+    if @question == 'I am going to work'
+      @answer = 'Great!'
+    elsif @question.end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
